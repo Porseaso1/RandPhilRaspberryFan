c84de3d0d63493c831371b6de5fbe7fca8cfd176
Porseaso1 <filippo.bisconcin@libero.it>
2015-05-27 19:02:42 +0200

    v1.1

--- fan.py
+++ fan.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python
-
 import RPi.GPIO as GPIO
 import sys
-
-pin = 22 #4 #11
-
+pin = 22
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
-
 state = True
-
 if(len(sys.argv) == 2):
 	if(sys.argv[1] == "on"):
 		GPIO.output(pin,True)
 	elif(sys.argv[1] ==  "off"):
 		GPIO.output(pin,False)
 	else:
-		print "Errore, on/off"
+		print "Error, on/off"
 else:
-	print "Errore"
+	print "Error"
